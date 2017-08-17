00238c57969ad9a10051b8938dfd7b4eae0ed800
archie11 <30882524+archie11@users.noreply.github.com>
2017-08-17 01:04:18 -0400

    First time website upload

---
+++ Learning Coding.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+   <head>
+      <title>Learning the Coding</title>
+<link href="C:\Users\Denis Buccat\Desktop\learningcoding.css" type="text/css" rel="stylesheet">
+        </head>
+   <body>
+      <marquee behavior="alternate" scrollamount="25"><h1>Coding is Fun</h1></marquee>
+     <h6>Author: Raffy Buccat <a href="C:\Users\Denis Buccat\Pictures\12291857_10203941961549788_7638155207050659105_o.jpg" target="_blank"><img alt="Raffy Buccat" src="C:\Users\Denis Buccat\Pictures\12291857_10203941961549788_7638155207050659105_o.jpg" height="42" width="50"/></a></h6>
+            <p>Finished lesson one of coding in <a target="_blank" href="https://www.codecademy.com/"><img src="https://www.hastac.org/sites/default/files/upload/images/post/picture.png" height="42" width="150" alt="Codacademy"/></a>. Phew!!!.Yehey.</p>
+      <p>So far, I learned:
+        <ul>
+           <li>what so called the boiler plate code</li>
+           <li>the declaration code "!DOCTYPE html"</li>
+           <li>the definition of html</li>
+           <li>the html tag</li> 
+          <li>the heading tag</li>
+          <li>the title tag</li>
+          <li>the body tag</li>
+          <li>that what shown in browser are in body tag</li>
+          <li>what is in the html element: <ol><li>the angel bracket</li><li>the opening</li><li> closing tag, and</li><li>the element</li></ol></li>
+         <br/>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+     </p>
+     <?php
+     $myage = 17
+     echo $myage
+  
+
+
+
+?>
+   </body>
+</html>
